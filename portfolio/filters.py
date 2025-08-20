846f4e0ea5e6ee86b02fa2db911a894031bd5d03
t3m14 <artemon.m174@gmail.com>
2025-08-20 19:29:50 +0500

    portfolio target filter

--- portfolio/filters.py
+++ portfolio/filters.py
@@ -45,7 +45,7 @@ class PortfolioFilter(django_filters.FilterSet):
         Фильтрация по target типов услуг или услуг
         """
         ServiceType = apps.get_model('service_types', 'ServiceType')
-        Service = apps.get_model('service', 'Service')
+        Service = apps.get_model('services', 'Service')
         
         # Получаем все ID типов услуг с указанным target
         service_type_ids = ServiceType.objects.filter(
